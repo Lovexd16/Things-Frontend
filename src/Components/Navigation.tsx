3b5f11dd6c20bffd606d1f0c8612ac32d9a866a3
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-09-24 13:49:36 +0200

    Navigeringsmeny
    Skapat en navigerings meny och 3 olika sidor, start, statistik, och lista.
    fixes #5

---
+++ src/Components/Navigation.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  setPage: (page: string) => void;
+}
+
+function Navigation(props: Props) {
+  return (
+    <div>
+      <button onClick={() => props.setPage("start")}>Startsida</button>
+      <button onClick={() => props.setPage("temperaturestatistics")}>
+        Temperatur statistik
+      </button>
+      <button onClick={() => props.setPage("listoftemperature")}>
+        Lista av temperatur värden
+      </button>
+    </div>
+  );
+}
+
+export default Navigation;
---
+++ src/Components/Pages/Start.tsx
@@ -0,0 +1,18 @@
+function Start() {
+  return (
+    <div>
+      <h1>Startsida</h1>
+      <p>
+        Detta är en sida skapad av Love för uppgiften Things. I denna uppgiften
+        ska man ta fram en "moln" uppkopplad sensor med ett Arduino kort. Mitt
+        mål med denna sida är att jag från mitt arduino kort mäter och skickar
+        temperatur värden från mitt rum till min Spring Boot backend, som jag
+        sedan ska hämta och använda i min ReactTS frontend. Jag vill skapa något
+        slags diagram med temperatur värdena på min statistik sida, och jag vill
+        lista ut dem senaste temperatur mätningarna på min andra sida.
+      </p>
+    </div>
+  );
+}
+
+export default Start;
